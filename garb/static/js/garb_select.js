32f70f4adcca07889a744366b0e6cb8607e694e2
Marcelo Gumercino Costa <mgc@MacBook-Pro-de-Marcelo.local>
2020-06-13 16:36:51 -0300

    add css checkbox + verify themes + remove bugs selects

--- garb/static/js/garb_select.js
+++ garb/static/js/garb_select.js
@@ -1,10 +1,20 @@
 (function ($) {
     // SELECTPICKER DATA-API
     // =====================
+
     function garb_select() { 
         $('select').each(function () {
-            var selectElement = $(this).attr("id").split('-');
-            if ( ! $('select').attr("multiple") && (selectElement[1] !== '__prefix__')) {
+            var prefixElement = true;
+            if($(this).prop("id")){
+                if ( $(this).attr("id").includes('-')) {
+                    var selectElement = $(this).attr("id").split('-');
+                    if ( selectElement[1] === '__prefix__') {
+                        prefixElement = false;
+                    }
+                }
+            }
+
+            if ( ! $('select').attr("multiple") && prefixElement) {
                 $.fn.selectpicker.Constructor.DEFAULTS.styleBase = null;
                 $.fn.selectpicker.Constructor.DEFAULTS.noneSelectedText = '---------'
                 $(this).selectpicker();
@@ -12,7 +22,10 @@
         });
     } 
     $(document).on("mousedown",'.add-row td a',function() {
-        setTimeout(function(){ garb_select()}, 100);
+        setTimeout(function(){ garb_select()}, 200);
+    });
+    $(document).on("mousedown",'div.add-row a',function() {
+        setTimeout(function(){ garb_select()}, 200);
     });
     garb_select();
 })(jQuery);
